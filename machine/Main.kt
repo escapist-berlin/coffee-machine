5ce070d19957e463b7a3003ef0e27df35495df4a
escapist-berlin <denis.kolokolov@icloud.com>
2025-09-23 19:58:23 +0200

    feat: Implement Stage 2: Ingredient calculator

--- machine/Main.kt
+++ machine/Main.kt
@@ -1,11 +1,22 @@
 package machine
 
+import java.util.Scanner
+
 fun main() {
-    println("Starting to make a coffee")
+    println("Write how many cups of coffee you will need:")
+    val scanner = Scanner(System.`in`)
+    val input = scanner.nextInt()
+
+    println("For $input cups of coffee you will need:")
+    println("${input * 200} ml of water")
+    println("${input * 50} ml of milk")
+    println("${input * 15} g of coffee beans")
+
+/*    println("Starting to make a coffee")
     println("Grinding coffee beans")
     println("Boiling water")
     println("Mixing boiled water with crushed coffee beans")
     println("Pouring coffee into the cup")
     println("Pouring some milk into the cup")
-    println("Coffee is ready!")
+    println("Coffee is ready!")*/
 }
